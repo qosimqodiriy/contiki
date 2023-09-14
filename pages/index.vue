fe5678334dba3642fe4da796d7e1884fdbc3f9ea
qosimqodiriy <qosimqodiriy701@gmail.com>
2023-09-14 16:36:39 +0500

    chabge meta tags

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,23 @@
 <template>
+    <Head>
+        <Title>Contiki</Title>
+        
+        <meta property="type" content="website">
+        <meta property="og:type" content="website">
+        
+        <meta property="url" content="https://contiki.uz">
+        <meta property="og:url" content="https://contiki.uz">
+        
+        <meta name="keywords" content="Sayohatlar, ekskursiya, dam olish joylari, tour, travel" />
+        <meta name="og:keywords" content="Sayohatlar, ekskursiya, dam olish joylari, tour, travel" />
+        
+        <meta name="description" content="Toshkentdan sayohatlar: raqobatbardosh narxda unutilmas sayohatlar" />
+        <meta name="og:description" content="Toshkentdan sayohatlar: raqobatbardosh narxda unutilmas sayohatlar" />
+        
+        <!-- <meta property="image" content="https://holdings.pythonanywhere.com/media/dropzone/business_multiple/2023/9/9/1694258864273484.png.900x900_q85.webp">
+        <meta property="og:image" content="https://holdings.pythonanywhere.com/media/dropzone/business_multiple/2023/9/9/1694258864273484.png.900x900_q85.webp"> -->
+    </Head>
+
     <section-one />
     <section-two />
     <section-three />
